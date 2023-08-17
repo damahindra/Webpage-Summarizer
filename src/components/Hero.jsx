5a632d9d519c3af17c7d040847402707561f1a0c
damahindra <ranggaandhito@gmail.com>
2023-08-17 23:31:37 +0700

    put logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import {logo} from '../assets'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center flex-row'>
+        <img src={logo} alt="logo" className='w-28 object-contain' />
+      </nav>
+    </header>
   )
 }
 
